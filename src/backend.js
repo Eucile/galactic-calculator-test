498d0928990eab12e338b24d964ac788c1a1f8a4
Krystal Foster <kmichelefoster@gmail.com>
2018-10-05 08:35:31 -0700

    setup empty class constructor and name all functions to be fulfilled

---
+++ src/backend.js
@@ -0,0 +1,43 @@
+export class Age {
+  constructor(age, lifeExpectancy) {
+    this.age - age;
+    this.lifeExpectancy = lifeExpectancy;
+  }
+
+  mercury() {
+
+  }
+
+  venus() {
+
+  }
+
+  mars() {
+
+  }
+
+  jupiter() {
+
+  }
+
+  mercurySpan() {
+
+  }
+
+  venusSpan() {
+
+  }
+
+  marsSpan() {
+
+  }
+
+  jupiterSpan() {
+
+  }
+
+  outlive() {
+
+  }
+
+}
